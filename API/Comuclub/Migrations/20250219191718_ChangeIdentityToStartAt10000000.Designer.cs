3b4b666d874522fee1fbb394a843a6a707da9c2e
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2025-02-19 22:38:52 +0300

    ID migration update

--- API/Comuclub/Migrations/20250219191718_ChangeIdentityToStartAt10000000.Designer.cs
+++ API/Comuclub/Migrations/20250219191718_ChangeIdentityToStartAt10000000.Designer.cs
@@ -3,6 +3,7 @@ using Comuclub.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Comuclub.Migrations
 {
     [DbContext(typeof(ComuClubContext))]
-    partial class ComuClubContextModelSnapshot : ModelSnapshot
+    [Migration("20250219191718_ChangeIdentityToStartAt10000000")]
+    partial class ChangeIdentityToStartAt10000000
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/Comuclub/Migrations/20250219191718_ChangeIdentityToStartAt10000000.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comuclub.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeIdentityToStartAt10000000 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "image",
+                table: "Events",
+                newName: "Image");
+
+            migrationBuilder.Sql("DBCC CHECKIDENT ('Organizers', RESEED, 9999999);");
+            migrationBuilder.Sql("DBCC CHECKIDENT ('Clubs', RESEED, 9999999);");
+            migrationBuilder.Sql("DBCC CHECKIDENT ('Events', RESEED, 9999999);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Events",
+                newName: "image");
+
+            migrationBuilder.Sql("DBCC CHECKIDENT ('Organizers', RESEED, 1);");
+            migrationBuilder.Sql("DBCC CHECKIDENT ('Clubs', RESEED, 1);");
+            migrationBuilder.Sql("DBCC CHECKIDENT ('Events', RESEED, 1);");
+
+        }
+    }
+}
